8727eab3ed39d9d11084fe69bcdd3597f4e0d1a1
overlookmotel <theoverlookmotel@gmail.com>
2014-11-28 21:51:26 +0000

    Tests

--- test/all.test.js
+++ test/all.test.js
@@ -10,7 +10,8 @@ var chai = require('chai'),
 	Support = require(__dirname + '/support'),
 	Sequelize = Support.Sequelize,
 	Promise = Sequelize.Promise,
-	_ = require('lodash');
+	_ = require('lodash'),
+	Queue = require('../lib/');
 
 // init
 chai.use(promised);
@@ -19,5 +20,41 @@ chai.config.includeStack = true;
 // tests
 
 describe(Support.getTestDialectTeaser('Tests'), function () {
-	
+	it('It works!', function() {
+		var results = [];
+		var queue = new Queue(this.sequelize, {
+			processors: {
+				default: function(data) {
+					return Promise.delay(100).then(function() {
+						results.push({b: data.a});
+						return {b: data.a};
+					});
+				}
+			}
+		});
+		
+		return queue.init()
+		.then(function() {
+			queue.start();
+			
+			return Promise.each([1, 2, 3], function(num) {
+				return queue.addJob({a: num});
+			})
+			.delay(500)
+			.then(function() {
+				expect(results).to.deep.equal([{b: 1}, {b: 2}, {b: 3}]);
+				
+				return queue.model.findAll({order: [['id']]})
+				.then(function(results) {
+					expect(results).to.be.ok;
+					expect(results.length).to.equal(3);
+					
+					results.forEach(function(result, index) {
+						var data = JSON.parse(result.result);
+						expect(data.b).to.equal(index + 1);
+					});
+				});
+			});
+		});
+	});
 });
